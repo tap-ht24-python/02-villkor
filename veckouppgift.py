5b00f714dedef3109fcc997222d238d36382eca0
David Andersson <lejonmanen@gmail.com>
2025-01-13 13:50:42 +0100

    Uppgift 2

---
+++ veckouppgift.py
@@ -0,0 +1,17 @@
+# Uppgift 2
+ticket_price = 100  # biljettpris
+money_on_hand = 200  # pengar på fickan
+
+#print("Det blir", (y - x), "kronor över.")
+print("Det blir " + str(money_on_hand - ticket_price) + " kronor över.")
+
+half_of_money_left = (200 - 100) / 2
+# operator precedence
+print("Hälften är: " + str(half_of_money_left))
+#print("Hälften är: ", z)
+
+# Bra variabelnamn:
+# - uttrycksfulla (beskriver vad de är till för)
+# - inga förkortningar
+# - lagom långa
+# - använd engelska konsekvent
