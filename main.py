0d5b44904465f8911a6b27263cc1b4fb074c2ac5
David Andersson <lejonmanen@gmail.com>
2025-01-13 15:02:04 +0100

    Bank-exempel

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello")
+account_balance = 0
+#greater_than_zero = account_balance > 0
+#print(greater_than_zero)
+
+if account_balance > 0:
+    # block i Python ska alltid vara indenterade
+    print("Du har pengar på kontot: " + str(account_balance))
+
+print("Nu tar banken semester...")
+
+
+
+#print(1 == 1)    # True, samma värde
+#print(2.0 == 2)  # Olika datatyp, men omvandlas till samma värde -> True
+#print("hej" == "HEJ")   # False, olika strängar
+#print(2 < 2)     # False
+
+
+
